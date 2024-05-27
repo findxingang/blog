aceaae577085e20ca7af202699954465a8cfbad6
xingang <findxingang@163.com>
2024-05-27 13:46:28 +0800

    feat: 自动化部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,8 +30,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://findxingang.github.io",
+  base: "/blog",
   trailingSlash: "always",
   integrations: [
     tailwind(),
